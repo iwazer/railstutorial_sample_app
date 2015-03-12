2fcbd48c47bdec81348efa999e45495cabe0e955
Eiji Iwazawa <iwazawa@gmail.com>
2015-03-12 22:21:51 +0900

    [WIP] list 7.26

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      redirect_to @user
     else
       render 'new'
     end
